ce51759dfd154304d1dea7dab78fa233803ff43a
Anton Statut <antonstatut@gmail.com>
2025-07-09 09:15:01 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let x;
+  let x2;
+  let d = b ** 2 - 4 * a * c;
+ 
+  if (d === 0){
+  x = -b / (2 * a);
+  arr.push(x);
+  } else if (d > 0){
+    x = (-b + Math.sqrt(d)) / (2 * a)
+    x2 = (-b - Math.sqrt(d)) / (2 * a)
+    arr.push(x, x2);
+  }
   return arr;
 }
+console.log(solveEquation());
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  let Payment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let totalAmount = Payment * countMonths;
+  totalAmount = totalAmount.toFixed(2);
+  let totalAmountAnswer = +totalAmount;
+  return totalAmountAnswer;
 }
+console.log(calculateTotalMortgage());
